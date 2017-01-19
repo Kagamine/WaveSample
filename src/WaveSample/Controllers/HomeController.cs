516e61f9fb3ae8f7afd4c987a5f4db26b2dfb57b
雨宮丶優子 <911574351@qq.com>
2017-01-19 13:26:38 +0800

    init

---
+++ src/WaveSample/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace WaveSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile File, int Count)
+        {
+            var memStream = new MemoryStream();
+            await File.CopyToAsync(memStream);
+            using (var wave = new WaveStream(memStream, 4, 42))
+            {
+                return View("Show", await wave.GetSampleAsync(Count));
+            }
+        }
+    }
+}
---
+++ src/WaveSample/Lib/WaveStream.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+
+namespace System
+{
+    public class WaveStream : IDisposable
+    {
+        public WaveStream(Stream stream, int ByteSample = 2, int DataPosition = 40)
+        {
+            _baseStream = stream;
+            _dataPosition = DataPosition;
+            _byteSample = ByteSample;
+            _length = (int)_baseStream.Length - DataPosition;
+        }
+
+        public WaveStream(string Path, int ByteSample = 2, int DataPosition = 40)
+            : this(new FileStream(Path, FileMode.Open, FileAccess.Read), ByteSample, DataPosition)
+        {
+        }
+
+        private Stream _baseStream;
+
+        public Stream BaseStream { get { return _baseStream; } }
+
+        private int _dataPosition, _byteSample, _length;
+        
+        private IEnumerable<double> _getSample(byte[] buffer, long cnt, long per)
+        {
+            for (var i = 0L; i < cnt; i+= per)
+            {
+                yield return BitConverter.ToInt32(buffer, (int)i) / 2147483648D;
+            }
+        }
+
+        public async Task<IEnumerable<double>> GetSampleAsync(int Count)
+        {
+            var cnt = _length / _byteSample;
+            var per = cnt / Count;
+            if (per % _byteSample != 0)
+                per = per - per % _byteSample;
+            var buffer = new byte[_length];
+            _baseStream.Position = _dataPosition;
+            await _baseStream.ReadAsync(buffer, 0, _length);
+            return _getSample(buffer, cnt, per);
+        }
+
+        public IEnumerable<double> GetSample(int Count)
+        {
+            var cnt = _length / _byteSample;
+            var per = cnt / Count;
+            var buffer = new byte[_length];
+            _baseStream.Position = _dataPosition;
+            _baseStream.Read(buffer, 0, _length);
+            return _getSample(buffer, cnt, per);
+        }
+
+        public void Dispose()
+        {
+            _baseStream.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
